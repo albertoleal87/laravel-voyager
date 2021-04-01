757179317a95fe1fe3060d0199f49940e71a1d16
Alberto Leal <betoleal87@gmail.com>
2021-03-31 20:10:40 -0600

    Print querys in log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,34 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        $this->LogQuerys();
+
+    }
+
+    public function LogQuerys()
+    {
+
+        \DB::listen(function($query){
+
+            $query_sql = $query->sql;
+            $bindings = $query->bindings;
+            $time = $query->time;
+
+            // Format binding data for sql insertion
+            foreach($bindings as $i => $binding){
+                if($binding instanceof \DateTime){
+                    $bindings[$i] = $binding->format('\'Y-m-d H:i:s\'');
+                }elseif(is_string($binding)){
+                    $bindings[$i] = "'$binding'";
+                }
+            }
+
+            // Insert bindings into query
+            $query_sql = str_replace(array('%', '?'), array('%%', '%s'), $query_sql);
+            $query_sql = vsprintf($query_sql, $bindings);
+            // print the query
+            \Log::info("[QUERY]: {$query_sql} [{$time}ms] ");    
+        });
+
     }
 }
